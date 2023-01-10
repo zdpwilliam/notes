f9b7a44f296f82f6e5bdb5e6916100e61f781558
william <zdpwilliam@gmail.com>
2023-01-10 10:34:21 +0800

    数据

--- language/python/db/BizDataWriteIn.py
+++ language/python/db/BizDataWriteIn.py
@@ -18,16 +18,19 @@ class BizData:
         self.bosId = int(BizData.find_field("bosId", fields))
         self.merchantId = int(BizData.find_field("merchantId", fields))
 
+    def to_tuple(self):
+        return (self.traceId, self.startTime, self.productId, self.openid, self.pid,
+                self.tcode, self.vid, self.wid, self.cid, self.bosId, self.merchantId)
+
     @staticmethod
     def find_field(field_name: str, fields: []):
         for field in fields:
             cur_field = str(field)
-            match_name = "%s:" + field_name
-            try:
-                if cur_field.find(match_name) > 0:
-                    return cur_field.replace(match_name, "")
-            except:
-                print('field_name --> %s,%s' % (field_name, fields))
+            match_name = "%s:" % field_name
+            if cur_field.find(match_name) < 0:
+                continue
+            else:
+                return cur_field.replace(match_name, "")
         return "0"
 
 
@@ -35,25 +38,26 @@ bizDataList = []
 csv_reader = csv.reader(open(
     "/Users/william/Spaces/gitlab/saas-cms-data-transfer/saas-cms-data-transfer-service/src/test/resources/traceBizData.csv"))
 for line in csv_reader:
-    bizDataList.append(BizData(line))
+    bizDataList.append(BizData(line).to_tuple())
 
 # 打开数据库连接
-db = pymysql.connect(host='localhost', user='root', password='', database='local-my-rpt')
+db = pymysql.connect(host='localhost', user='root', password='', database='local-my-rpt', charset='utf8')
+
+#事务开启
+db.begin()
 # 使用 cursor() 方法创建一个游标对象 cursor
 cursor = db.cursor()
 # SQL 插入语句
 sql = "insert into biz_data_rpt (traceId, starTime, productId, openid, pid, tcode, vid, wid, merchantId, bosId, cid)" \
-      " values ('%s', '%s', %s, '%s', %s, '%s', %s, %s, %s, %s, %s);"
+      " values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
 try:
-    for bizData in bizDataList:
-        oneIn = sql % (bizData.traceId, bizData.startTime, bizData.productId, bizData.openid, bizData.pid,
-                       bizData.tcode, bizData.vid, bizData.wid, bizData.merchantId, bizData.bosId, bizData.cid)
-        # 执行sql语句
-        cursor.execute(sql)
+    # 执行sql语句
+    cursor.executemany(sql, bizDataList)
     # 提交到数据库执行
     db.commit()
-except:
+except Exception as e:
     # 如果发生错误则回滚
+    print("error --> ", e)
     db.rollback()
 
 # 关闭数据库连接
